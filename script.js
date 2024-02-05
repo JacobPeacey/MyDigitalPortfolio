9460aa4d3aee4449af768f614f7438c85bdf306b
JacobPeacey <10693315@student.bpc.ac.uk>
2024-02-05 16:20:01 +0000

    Bug fixes and improvements
    This commit adds pages  and descriptions for individual units, and fixes the navbar on some pages

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     const prefixElement = document.getElementById('prefix-text');
     const contentElement = document.getElementById('content-text');
+
+    if (!prefixElement || !contentElement) {
+        console.error("Couldn't find required HTML elements. Make sure 'prefix-text' and 'content-text' elements exist.");
+        return;
+    }
+
     const textContents = [
         "App Store Developer.",
         "Student.",
         "Swift Student Challenge Contender.",
         "Keen Coder.",
     ];
-    const prefixText = "I'm an";
+    const prefixText = "I'm a";
     let contentIndex = 0;
     let charIndex = 0;
     let isDeleting = false;
@@ -26,31 +31,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (!isDeleting && charIndex <= currentText.length) {
-            setTimeout(() => type(), 50); // Typing speed
+            setTimeout(type, 50); // Typing speed
         } else if (!isDeleting && charIndex > currentText.length) {
             isDeleting = true;
-            setTimeout(() => type(), 1000); // Delay before starting the delete animation
+            setTimeout(type, 1000); // Delay before starting the delete animation
         } else if (isDeleting && charIndex > 0) {
             isDeleting = false;
-            setTimeout(() => type(), 50); // Typing speed during delete animation
+            setTimeout(type, 50); // Typing speed during delete animation
         } else {
             contentIndex = (contentIndex + 1) % textContents.length; // Loop back to the first text if reached the end
             isDeleting = false;
-            setTimeout(() => type(), 1500); // Delay before typing the next text
+            setTimeout(type, 1500); // Delay before typing the next text
         }
     }
 
     type();
 });
-
-
-
-
-
-
-
-
-
-
-
-
